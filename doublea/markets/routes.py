2657a8a0483483c866f78d5a9a9d50b9b2a4934b
Allan Anderson <allana@shaw.ca>
2026-03-21 10:42:14 -0600

    Update to include selected market

--- doublea/markets/routes.py
+++ doublea/markets/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import login_required
+from flask import Blueprint, abort, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
 from doublea.markets.forms import MarketForm
-from doublea.models import Market
+from doublea.models import Market, MarketSales
 
 from doublea import db
 
@@ -24,3 +24,18 @@ def new_market():
         flash('Your Market has been created!', 'success')
         return redirect(url_for('markets.market_management'))
     return render_template('new_market.html',tittle='New Market',form=form, legend='New Market')
+
+@markets.route("/market/<int:market_id>")
+@login_required
+def market(market_id):
+    if current_user.is_authenticated:
+        page = request.args.get('page', 1, type=int)
+        one_market= Market.query.filter_by(marketsid=market_id).first_or_404()
+        market_sales = MarketSales.query.filter_by(marketid=market_id)\
+          .order_by(MarketSales.marketdate.desc()).paginate(page=page, per_page=10)
+        return render_template('market.html',
+                           title=one_market.market,
+                           one_market=one_market,
+                           market_sales=market_sales)
+    else:
+        abort(403)
--- doublea/stores/routes.py
+++ doublea/stores/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import login_required
+from flask import Blueprint, abort, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
 from doublea.models import Store, Transaction
 
 from doublea import db
@@ -26,12 +26,16 @@ def new_store():
     return render_template('new_store.html',tittle='New Store',form=form, legend='New Store')
 
 @stores.route("/store/<int:store_id>")
+@login_required
 def store(store_id):
-    page = request.args.get('page', 1, type=int)
-    one_store= Store.query.filter_by(storeid=store_id).first_or_404()
-    store_transactions = Transaction.query.filter_by(storeid=store_id)\
-        .order_by(Transaction.transactiondate.desc()).paginate(page=page, per_page=10)
-    return render_template('store.html',
+    if current_user.is_authenticated:
+        page = request.args.get('page', 1, type=int)
+        one_store= Store.query.filter_by(storeid=store_id).first_or_404()
+        store_transactions = Transaction.query.filter_by(storeid=store_id)\
+            .order_by(Transaction.transactiondate.desc()).paginate(page=page, per_page=10)
+        return render_template('store.html',
                            title=one_store.storename,
                            one_store=one_store,
                            store_transactions=store_transactions)
+    else:
+        abort(403)
